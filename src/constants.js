bf4b80b4b4889548ccbc56466adc760887f8d897
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-26 09:46:35 +0600

    Add JSDoc for export

--- src/constants.js
+++ src/constants.js
@@ -31,6 +31,7 @@ export const DEFAULT_SHIP_TIER = 'BASIC';
 // Initial Balancing
 /** JSDoc for exported member */
 export const INITIAL_SPAWN_RATE = 90;
+/** JSDoc for exported member */
 export const MIN_SPAWN_RATE = 20;
 export const INITIAL_ASTEROID_SPEED = 2;
 export const SHOCKWAVE_COOLDOWN = 300;
