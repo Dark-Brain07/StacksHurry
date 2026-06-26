0f0128ebfcacaa00c159ab370eea158ed87211cd
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-26 09:46:36 +0600

    Add JSDoc for export

--- src/constants.js
+++ src/constants.js
@@ -33,6 +33,7 @@ export const DEFAULT_SHIP_TIER = 'BASIC';
 export const INITIAL_SPAWN_RATE = 90;
 /** JSDoc for exported member */
 export const MIN_SPAWN_RATE = 20;
+/** JSDoc for exported member */
 export const INITIAL_ASTEROID_SPEED = 2;
 export const SHOCKWAVE_COOLDOWN = 300;
 export const SHOCKWAVE_RADIUS = 250;
