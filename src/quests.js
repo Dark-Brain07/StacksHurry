a8975d4780ab308e5755d29aea5321123437f048
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-26 09:46:47 +0600

    Add param tag to if

--- src/quests.js
+++ src/quests.js
@@ -451,6 +451,7 @@ export function initQuestListeners(onQuestCompletedCallback, onQuestMilestoneCal
 
   QuestsEventDispatcher.subscribe('shockwaveDeflected', () => {
     const completed = updateQuestProgress(QUEST_TYPES.SHOCKWAVE_DEFLECT, 1);
+/** @param {any} param */
     if (completed && completed.length > 0 && questCompletedCallback) {
       completed.forEach(q => questCompletedCallback(q));
     }
