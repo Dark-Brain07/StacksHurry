3005e47c5aeb3875b0068a323840404f8c7e7668
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-28 07:10:04 +0600

    Add constant tag to result

--- src/contracts.js
+++ src/contracts.js
@@ -170,6 +170,7 @@ export async function getPlayerScore(playerAddress) {
 
 /** Get player's high score only */
 export async function getPlayerHighScore(playerAddress) {
+/** @constant {any} */
   const result = await readOnly(
     CONTRACTS.ROCKET_SHOOTER,
     'get-high-score',
