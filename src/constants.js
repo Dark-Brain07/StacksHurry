18b9b6e0d8efc52dafa4a9dc42a599d13c950639
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-28 07:10:25 +0600

    Add constant tag to SHOCKWAVE_COOLDOWN

--- src/constants.js
+++ src/constants.js
@@ -46,6 +46,7 @@ export const MIN_SPAWN_RATE = 20;
 /** JSDoc for exported member */
 export const INITIAL_ASTEROID_SPEED = 2;
 /** JSDoc for exported member */
+/** @constant {any} */
 export const SHOCKWAVE_COOLDOWN = 300;
 /** JSDoc for exported member */
 export const SHOCKWAVE_RADIUS = 250;
