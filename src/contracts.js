43ad9ebc5f90a58a7b1da6d8bdf01e84333ec6f3
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-28 07:10:42 +0600

    Add constant tag to result

--- src/contracts.js
+++ src/contracts.js
@@ -191,6 +191,7 @@ export async function getPlayerCount() {
 
 /** Get games played for a player */
 export async function getGamesPlayed(playerAddress) {
+/** @constant {any} */
   const result = await readOnly(
     CONTRACTS.ROCKET_SHOOTER,
     'get-games-played',
