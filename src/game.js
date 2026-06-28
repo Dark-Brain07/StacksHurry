5604fccbd558e824b9a0f8eeb30e6d0a9a1d3a09
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-28 07:10:12 +0600

    Add description to if

--- src/game.js
+++ src/game.js
@@ -330,6 +330,7 @@ function handleTouchStart(e) {
   for (let i = 0; i < e.changedTouches.length; i++) {
     const touch = e.changedTouches[i];
 /** @param {any} param */
+/** @description if logic */
     if (touch.clientX < window.innerWidth / 2) {
       joystick.active = true;
       joystick.startX = touch.clientX;
