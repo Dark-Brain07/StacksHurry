b45b7e884b9bea37205df0ebc34ee95e32158670
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-28 07:09:47 +0600

    Add constant tag to gain

--- src/audio.js
+++ src/audio.js
@@ -279,6 +279,7 @@ export function playWarning() {
   try {
     const ctx = getCtx();
     const osc = ctx.createOscillator();
+/** @constant {any} */
     const gain = ctx.createGain();
     osc.connect(gain);
     gain.connect(ctx.destination);
