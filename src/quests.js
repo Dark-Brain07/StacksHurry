009397e41ad654d681be4482ecf67f5ab6409eaa
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-28 07:10:03 +0600

    Add constant tag to completed

--- src/quests.js
+++ src/quests.js
@@ -455,6 +455,7 @@ export function initQuestListeners(onQuestCompletedCallback, onQuestMilestoneCal
   });
 
   QuestsEventDispatcher.subscribe('waveCleared', () => {
+/** @constant {any} */
     const completed = updateQuestProgress(QUEST_TYPES.SURVIVE_WAVES, 1);
 /** @param {any} param */
     if (completed && completed.length > 0 && questCompletedCallback) {
