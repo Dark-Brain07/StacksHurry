99e16872a249aa72323558d4f7746c6fddb93467
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-28 07:10:30 +0600

    Add constant tag to result

--- src/contracts.js
+++ src/contracts.js
@@ -313,6 +313,7 @@ export async function getPilotByNameOnChain(name) {
 
 /** Get total registered pilots */
 export async function getTotalPilots() {
+/** @constant {any} */
   const result = await readOnly(CONTRACT_PILOT_REGISTRY, 'get-total-pilots');
   return typeof result === 'number' ? result : (result || 0);
 }
