596c4d92021fc770e3ff1762a396ca7534af82fd
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-28 07:10:24 +0600

    Add type to lastTouchTime

--- src/game.js
+++ src/game.js
@@ -128,6 +128,7 @@ export function setSettings(settings) {
 }
 let secondaryCooldown = 0;
 let shockwave = { active: false, x: 0, y: 0, radius: 0 };
+/** @type {any} */
 let lastTouchTime = 0;
 let warpTime = 0;
 let achievements = { score1k: false, score5k: false, level5: false, asteroids50: false };
