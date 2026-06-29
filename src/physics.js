c2c98d2b30ef454850b8235c1c7b94ac5ad781c4
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-29 10:24:49 +0600

    Add author tag to lerp

--- src/physics.js
+++ src/physics.js
@@ -259,6 +259,7 @@ export function distance(x1, y1, x2, y2) {
  * Linear interpolation
  */
 /** @description lerp logic */
+/** @author Dark-Brain07 */
 export function lerp(start, end, amt) {
   return (1 - amt) * start + amt * end;
 }
