4d2399f86ead992b9fff2c3fe0c0177619dadf46
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-29 10:25:03 +0600

    Add version tag to SHIP_THEMES

--- src/constants.js
+++ src/constants.js
@@ -77,6 +77,7 @@ export const COLORS = {
 // Ship Accent Themes
 /** JSDoc for exported member */
 /** @constant {any} */
+/** @version 1.2.4 */
 export const SHIP_THEMES = {
   vanguard: {
     id: 'vanguard',
